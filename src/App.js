1c4d6ac5782a533700a4d7368feb9717d00fe6db
Joseph Rivera <josephr98@gmail.com>
2020-04-09 11:19:05 -0400

    added src/

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import List from './List';
 
-function App() {
+function App(props) {
+  const STORE = props.store;
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trelloyes!</h1>
       </header>
-    </div>
+      <div className="App-list">
+        {STORE.lists.map(list => (
+          <List 
+            key={list.id}
+            header={list.header}
+            cards={list.cardIds.map(id => STORE.allCards[id])}
+          />
+        ))}
+      </div>
+    </main>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={Store.STORE} />
   </React.StrictMode>,
   document.getElementById('root')
 );
